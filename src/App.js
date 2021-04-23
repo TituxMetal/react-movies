35435149f4cd21bdcc65d569fa8df015ed354adc
Titux Metal <github@lgdweb.fr>
2021-04-23 08:10:14 +0200

    Updating App.js file; handling http errors

--- src/App.js
+++ src/App.js
@@ -6,20 +6,32 @@ import './App.css'
 const App = () => {
   const [movies, setMovies] = useState([])
   const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState(null)
 
   const fetchMoviesHandler = async () => {
     setIsLoading(true)
-    const response = await fetch('https://swapi.dev/api/films/')
-    const data = await response.json()
-    const moviesData = data.results.map(movie => ({
-      id: movie.episode_id,
-      title: movie.title,
-      openingText: movie.opening_crawl,
-      releaseDate: movie.release_date
-    }))
+    setError(null)
 
+    try {
+      const response = await fetch('https://swapi.dev/api/films/')
+      if (!response.ok) {
+        throw new Error(
+          `Something went wrong! Status: ${response.status} Message: ${response.statusText}`
+        )
+      }
+      const data = await response.json()
+      const moviesData = data.results.map(movie => ({
+        id: movie.episode_id,
+        title: movie.title,
+        openingText: movie.opening_crawl,
+        releaseDate: movie.release_date
+      }))
+
+      setMovies(moviesData)
+    } catch (err) {
+      setError(err.message)
+    }
     setIsLoading(false)
-    setMovies(moviesData)
   }
 
   return (
@@ -28,8 +40,9 @@ const App = () => {
         <button onClick={fetchMoviesHandler}>Fetch movies</button>
       </section>
       <section>
+        {error && <p>{error}</p>}
         {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
-        {!isLoading && movies.length === 0 && <p>No movies found</p>}
+        {!isLoading && !error && movies.length === 0 && <p>No movies found</p>}
         {isLoading && <p>Loading...</p>}
       </section>
     </>
