1fe308d84142f524eb30f096a8df87be2dd26949
Titux Metal <github@lgdweb.fr>
2021-04-23 07:25:39 +0200

    Updating App.js file; handling loading and data state

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ import './App.css'
 
 const App = () => {
   const [movies, setMovies] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
 
   const fetchMoviesHandler = async () => {
+    setIsLoading(true)
     const response = await fetch('https://swapi.dev/api/films/')
     const data = await response.json()
     const moviesData = data.results.map(movie => ({
@@ -16,6 +18,7 @@ const App = () => {
       releaseDate: movie.release_date
     }))
 
+    setIsLoading(false)
     setMovies(moviesData)
   }
 
@@ -25,7 +28,9 @@ const App = () => {
         <button onClick={fetchMoviesHandler}>Fetch movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && <p>No movies found</p>}
+        {isLoading && <p>Loading...</p>}
       </section>
     </>
   )
